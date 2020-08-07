cf6afd29d773b59c133d1b64f14f914b406bd1bd
saltekar <sahil.altekar1@gmail.com>
2020-08-06 17:07:28 -0700

    Started story engine

--- app/assets/story.js
+++ app/assets/story.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { thistle } from "color-name";
 
-export const story = [
+let story = [
   "Felix... Felix! Wake up!",
   "We have to go NOW!",
   "You wake up slowly in a daze.",
@@ -8,3 +9,38 @@ export const story = [
   "HEY. who's there?",
   "Where am I?"
 ];
+
+let d1 = ["Don't worry about who I am."];
+
+let d2 = ["We in the middle of the forest."];
+
+class Node {
+  constructor(content, name) {
+    this.name = name;
+    this.content = content;
+    this.nextNodes = [];
+  }
+
+  addNode(node) {
+    this.nextNodes.push(node);
+  }
+
+  printNeigbors() {
+    let arr = [];
+
+    for (let x = 0; x < this.nextNodes.length; x++) {
+      arr.push(this.nextNodes[x].name);
+    }
+
+    return this.name + " -> " + arr;
+  }
+}
+
+const A = new Node(story, "A");
+const B = new Node(d1, "B");
+const C = new Node(d2, "C");
+
+A.addNode(B);
+A.addNode(C);
+
+console.log(B.printNeigbors());
